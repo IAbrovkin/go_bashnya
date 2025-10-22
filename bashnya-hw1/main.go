35c1ea79b7991cbd0cbf631bfe6c4b84fae5752f
IAbrovkin <brovkin.a.1506@gmail.com>
2025-10-22 10:46:27 +0300

    First homework

---
+++ bashnya-hw1/main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	name := "Vanya"
+	course := 2
+	fmt.Println("Hello, it is the first homework of Bashnya")
+	fmt.Printf("My name is %s. I am a %d-year student in the field of ICT - Information and Communication Technologies and Systems at the Higher School of Economics.", name, course)
+}
